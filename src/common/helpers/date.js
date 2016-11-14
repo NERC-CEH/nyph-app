63dbd58931c54630e07316bc31ab2b1bc4e23281
Tom Humphrey <tom.humphrey@bsbi.org>
2016-11-14 13:37:57 +0000

    format dates with time, using stamp

--- src/common/helpers/date.js
+++ src/common/helpers/date.js
@@ -14,4 +14,35 @@ export default {
     const local = new Date(date);
     return `${local.getDate()}/${(local.getMonth() + 1)}/${local.getFullYear()}`;
   },
+  
+  prettyPrintStamp(recordModel) {
+    const recordDateString = recordModel.get('date');
+    
+    if (recordDateString) {
+      const recordDate = new Date(recordDateString);
+      
+      if (recordDate) {
+        const recordStamp = new Date(recordModel.get('entry_time'));
+
+        const location = recordModel.get('location');
+        const gpsed = location && location.source === 'gps';
+
+        const today = new Date();
+
+        const todayDateOnly = `${today.getDate()}/${(today.getMonth() + 1)}/${today.getFullYear()}`;
+        const recordDateOnly = `${recordDate.getDate()}/${(recordDate.getMonth() + 1)}/${recordDate.getFullYear()}`;
+        const stampDateOnly = `${recordStamp.getDate()}/${(recordStamp.getMonth() + 1)}/${recordStamp.getFullYear()}`;
+
+        const isToday = (todayDateOnly === recordDateOnly);
+        if (gpsed && (recordDateOnly === stampDateOnly)) {
+          // trust that the entry stamp time reflects the field record time
+          
+          return (isToday ? 'today' : recordDateOnly);
+        } else {
+          return `${isToday ? 'today' : recordDateOnly}, ${recordStamp.getHours()}:${recordStamp.getMinutes()}`;
+        }
+      } 
+    } 
+    return '';
+  },
 };
