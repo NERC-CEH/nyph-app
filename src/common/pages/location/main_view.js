e1669c771ba737948abd627543e0ded48ecf2f92
Tom Humphrey <tom.humphrey@bsbi.org>
2016-11-16 00:18:52 +0000

    consistent treatment of accuaracy, as metres

--- src/common/pages/location/main_view.js
+++ src/common/pages/location/main_view.js
@@ -471,7 +471,8 @@ const LocationView = Marionette.View.extend({
     this.updateMapMarker(location);
     
     //if source was 'map' then presume that current zoom is fine so don't change (send undefined)
-    this.map.setView(this._getCenter(), location.source !== 'map' ? this._getZoomLevel() : undefined);
+    //this.map.setView(this._getCenter(), location.source !== 'map' ? this._getZoomLevel() : undefined);
+    this.map.setView(this._getCenter(), this._getZoomLevel());
     this._refreshGridRefElement(location);
   },
 
