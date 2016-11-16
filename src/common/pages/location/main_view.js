7b8d820f466834f96123e065c3becb7daa96195e
Tom Humphrey <tom.humphrey@bsbi.org>
2016-11-16 01:39:46 +0000

    try zooming afterwards

--- src/common/pages/location/main_view.js
+++ src/common/pages/location/main_view.js
@@ -468,14 +468,13 @@ const LocationView = Marionette.View.extend({
     
     this._refreshGrErrorState(false);
     
-   
+    this.updateMapMarker(location);
     
     //if source was 'map' then presume that current zoom is fine so don't change (send undefined)
     //this.map.setView(this._getCenter(), location.source !== 'map' ? this._getZoomLevel() : undefined);
-    this.map.setView(this._getCenter(), this._getZoomLevel());
+    //this.map.setView(this._getCenter(), this._getZoomLevel());
+    this.map.panTo(this._getCenter());
     this.map.setZoom(this._getZoomLevel());
-    
-    this.updateMapMarker(location);
     
     this._refreshGridRefElement(location);
   },
