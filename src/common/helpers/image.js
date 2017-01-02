6ed438776d31e6585086e6cdadf422160db05cfa
Tom Humphrey <tom.humphrey@bsbi.org>
2017-01-02 09:57:01 +0000

    camera resolution

--- src/common/helpers/image.js
+++ src/common/helpers/image.js
@@ -41,7 +41,7 @@ const Image = {
       sourceType: window.Camera.PictureSourceType.CAMERA,
       // allow edit is unpredictable on Android and it should not be used!
       allowEdit: false,
-      quality: 40,
+      quality: 80, // was 40
       targetWidth: 1000,
       targetHeight: 1000,
       destinationType: window.Camera.DestinationType.FILE_URI,
